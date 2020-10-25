555db7ff19ad1e46d1d24895948b44663204d15e
Grace Kuo <gkuo.github@gmail.com>
2020-10-25 17:19:48 -0600

    creating click and save function for calendar events

--- Develop/calendar.js
+++ Develop/calendar.js
@@ -1,7 +1,9 @@
 //Telling the js to run the html and then css:
 $(document).ready(function () {
   $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-  var now = moment();
 
-  console.log(now);
+  //creating save button for calendar events
+  $(".save").on("click", function () {
+    localStorage.setItem(time, text);
+  });
 });
